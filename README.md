e5726f230a2acb607ea48da0b908c07b82d0e58a
rogeliogamez <rogeliogamez92@gmail.com>
2023-02-18 17:30:53 -0500

    Test console app

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# blog-multiple-apsettings
-Using multiple appsettings.json to release to different platforms in dotnet
+# Using multiple appsettings.json to release to different platforms in dotnet
+
+Please follow [Using multiple appsettings.json to release to different platforms in dotnet](https://dev.to/rogeliogamez92/using-multiple-appsettingsjson-to-release-to-different-platforms-in-dotnet-2554).
---
+++ ReleaseSettings/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var configurationBuilder = new ConfigurationBuilder();
+
+configurationBuilder.AddJsonFile("appsettings.json");
+
+#if DEBUG
+configurationBuilder.AddJsonFile("appsettings.debug.json");
+#elif AZURE
+configurationBuilder.AddJsonFile("appsettings.azure.json");
+#elif LINUX
+configurationBuilder.AddJsonFile("appsettings.linux.json");
+#endif
+
+configurationBuilder.AddJsonFile("appsettings.user.json");
+
+var configuration = configurationBuilder.Build();
+
+Console.WriteLine(configuration.GetValue<string>("GlobalSetting"));
+Console.WriteLine(configuration.GetValue<string>("PlatformSetting"));
+Console.WriteLine(configuration.GetValue<string>("UserSetting"));
